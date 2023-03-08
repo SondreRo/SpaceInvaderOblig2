bb19838d36ecac7a48e300913156a8694b275c79
Sondre R <soroensg@gmail.com>
2023-03-08 17:52:36 +0100

    Finished Game

--- Source/SpaceInvader/Public/GameController.h
+++ Source/SpaceInvader/Public/GameController.h
@@ -13,5 +13,52 @@ UCLASS()
 class SPACEINVADER_API AGameController : public AGameModeBase
 {
 	GENERATED_BODY()
+<<<<<<< Updated upstream
 	
+=======
+
+public:
+	AGameController();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+	TArray<int> WaveSize;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+	TArray<int> WaveDifficulty;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+	TArray<float> WaveFrequency;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+	float xPos;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+	float Min_yPos;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+	float Max_yPos;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+	float Min_zPos;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+	float Max_zPos;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, category = "My Variables")
+	int WaveNumber;
+
+	UPROPERTY(EditAnywhere ,BlueprintReadWrite, Category = "My Variables")
+	TSubclassOf<AEnemy> Enemy_BP;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+	bool GameWon;
+
+
+
+private:
+	int EnemyNumbers;
+	float timer;
+
+
+	void ChangeWave(int Wave);
+>>>>>>> Stashed changes
 };
